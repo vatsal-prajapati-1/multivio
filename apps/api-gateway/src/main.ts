a769c42540a5d6e22a939df81669f902b87d6ee9
Vatsal Prajapati <prajapativatsal555@gmail.com>
2025-08-12 23:19:52 +0530

    feat: update package.json with new scripts and dependencies
    feat: add error handling middleware and custom error classes
    
    feat: implement Prisma client setup for MongoDB
    
    feat: add Redis client setup
    
    feat: create Prisma schema for images and users models
    
    chore: update tsconfig.base.json to include baseUrl and paths
    
    chore: update tsconfig.json to include api-gateway in paths

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,49 @@
+import express, { Request } from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  })
+);
+
+app.use(morgan('dev'));
+app.use(express.json({ limit: '1000mb' }));
+app.use(express.urlencoded({ limit: '1000mb', extended: true }));
+app.use(cookieParser());
+app.set('trust proxy', 1);
+
+// Apply rate limiting
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: (req: any) => (req.user ? 1000 : 100),
+  message: { error: 'Too many request, please try again later!' },
+  standardHeaders: true,
+  legacyHeaders: true,
+  keyGenerator: (req: Request) => ipKeyGenerator(req.ip ?? ''),
+});
+
+app.use(limiter);
+
+app.get('/gateway-health', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
+});
+
+app.use('/', proxy('http://localhost:6001'));
+
+const port = process.env.PORT || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/gateway-health`);
+});
+server.on('error', console.error);
---
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -0,0 +1,198 @@
+import { NextFunction, Request, Response } from 'express';
+import {
+  checkOtpRestrictions,
+  handleForgotPassword,
+  sendOtp,
+  trackOtpRequests,
+  validateRegistrationData,
+  verifyForgotPasswordOtp,
+  verifyOtp,
+} from '../utils/auth.helper';
+import prisma from '@packages/libs/prisma';
+import { AuthenticationError, ValidationError } from '@packages/error-handler';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { setCookie } from '../utils/cookies/setCookie';
+
+// Register a new user
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    validateRegistrationData(req.body, 'user');
+
+    const { name, email } = req.body;
+
+    const existingUser = await prisma.users.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return next(new ValidationError('User already exists with this email!'));
+    }
+
+    await checkOtpRestrictions(email, next);
+    await trackOtpRequests(email, next);
+    await sendOtp(name, email, 'user-activation-mail');
+
+    res.status(200).json({
+      message: 'OTP sent to email. Please verify your account.',
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// verify user with otp
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, otp, password, name } = req.body;
+
+    if (!email || !otp || !password || !name) {
+      return next(new ValidationError('All fields are required!'));
+    }
+
+    const existingUser = await prisma.users.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return next(new ValidationError('User already exists with this email!'));
+    }
+
+    await verifyOtp(email, otp, next);
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.users.create({
+      data: { name, email, password: hashedPassword },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: 'User registered successfully!',
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// login user
+export const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return next(new ValidationError('Email and Password are required!'));
+    }
+
+    const user = await prisma.users.findUnique({ where: { email } });
+
+    if (!user) return next(new AuthenticationError("User doesn't exists!"));
+
+    // verify password
+
+    const isMatch = await bcrypt.compare(password, user.password!);
+
+    if (!isMatch) {
+      return next(new AuthenticationError('Invalid email or password'));
+    }
+
+    // Generate access and refresh token
+    const accessToken = jwt.sign(
+      { id: user.id, role: 'user' },
+      process.env.ACCESS_TOKEN_SECRET as string,
+      {
+        expiresIn: '15m',
+      }
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user.id, role: 'user' },
+      process.env.REFRESH_TOKEN_SECRET as string,
+      {
+        expiresIn: '7d',
+      }
+    );
+
+    // store the refresh and access token in an httpOnly secure cookie
+
+    setCookie(res, 'refresh_token', refreshToken);
+    setCookie(res, 'access_token', accessToken);
+
+    res.status(200).json({
+      message: 'Login successful!',
+      user: { id: user.id, email: user.email, name: user.name },
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// user forgot password
+export const userForgotPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  await handleForgotPassword(req, res, next, 'user');
+};
+
+// Verify forgot password OTP
+
+export const verifyUserForgotPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  await verifyForgotPasswordOtp(req, res, next);
+};
+
+// Reset user password
+export const resetUserPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, newPassowrd } = req.body;
+
+    if (!email || !newPassowrd) {
+      return next(new ValidationError('Email and new password are required!'));
+    }
+
+    const user = await prisma.users.findUnique({ where: { email } });
+
+    if (!user) return next(new ValidationError('User not found!'));
+
+    // compare new password with the existing one
+
+    const isSamePassword = await bcrypt.compare(newPassowrd, user.password!);
+
+    if (isSamePassword) {
+      return next(
+        new ValidationError(
+          'New password cannot be the same as the old password!'
+        )
+      );
+    }
+
+    // hash the new password
+
+    const hashedPassword = await bcrypt.hash(newPassowrd, 10);
+
+    await prisma.users.update({
+      where: { email },
+      data: { password: hashedPassword },
+    });
+
+    res.status(200).json({ message: 'Password reset successfully!' });
+  } catch (error) {
+    next(error);
+  }
+};
