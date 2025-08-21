aebe62707e89536cbc5dfd5dc530ebbde719ac67
vatsal prajapati <prajapativatsal555@gmail.com>
2025-08-21 17:20:53 +0530

    feat: add site configuration initialization and update main application logic
    - Introduced a new `initializeSiteConfig` function to set up default site configuration in the database if it doesn't already exist.
    - Updated `main.ts` to call `initializeSiteConfig` on server start, logging success or error messages accordingly.
    - Modified Prisma schema to include a new `site_config` model for managing site categories and subcategories.
    - Enhanced the create-product page with a new dropdown for cash on delivery options.

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,6 +6,7 @@ import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
 import swaggerUi from 'swagger-ui-express';
 import axios from 'axios';
 import cookieParser from 'cookie-parser';
+import initializeSiteConfig from './libs/initializeSiteConfig';
 
 const app = express();
 
@@ -45,5 +46,11 @@ app.use('/', proxy('http://localhost:6001'));
 const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/gateway-health`);
+  try {
+    initializeSiteConfig()
+    console.log("Site config initialized successfully!")
+  } catch (error) {
+    console.error("❌ Failed to initialize site config:", error)
+  }
 });
 server.on('error', console.error);
--- apps/seller-ui/src/app/(routes)/dashboard/create-product/page.tsx
+++ apps/seller-ui/src/app/(routes)/dashboard/create-product/page.tsx
@@ -233,6 +233,39 @@ const Page = () => {
               <div className="mt-2">
                 <CustomProperties control={control} errors={errors} />
               </div>
+
+              <div className="mt-2">
+                <label className="block font-semibold text-gray-300 mb-1">
+                  Cash On Delivery *
+                </label>
+
+                <select
+                  {...register('cash_on_delivery', {
+                    required: 'Cash on Delivery is required',
+                  })}
+                  defaultValue="yes"
+                  className="w-full border outline-none border-gray-700 bg-transparent"
+                >
+                  <option value="yes" className="bg-black">
+                    Yes
+                  </option>
+                  <option value="no" className="bg-black">
+                    No
+                  </option>
+                </select>
+
+                {errors.cash_on_delivery && (
+                  <p className="text-red-500 text-xs mt-1">
+                    {errors.cash_on_delivery.message as string}
+                  </p>
+                )}
+              </div>
+            </div>
+
+            <div className="w-2/4">
+              <label className="block font-semibold text-gray-300 mb-1">
+                Category
+              </label>
             </div>
           </div>
         </div>
